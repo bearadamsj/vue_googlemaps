70943b15445621d09ae5bcb34acab6123ac5da06
zhengran <barodaslife@gmail.com>
2023-06-14 16:30:13 +0800

    thire commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,6 @@
 import requests from "./request";
+
+// get timezone api
 export const reqTimeZone = (lat, lng, key) => {
     return requests({
         url: `get-time-zone?key=${key}&format=json&by=position&lat=${lat}&lng=${lng}`,
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  // proxy server for change of Origin
   devServer:{
+
     proxy: {
+      //timezone proxy server
       '/get-time-zone':{
         secure: false,
         changeOrigin: true, 
